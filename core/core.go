46b18bf86c7bb31aae48accceb3b54ba92df925c
Peter A. Ramaldes <peter.ramaldes@gmail.com>
2021-01-09 13:48:58 -0300

    [#9] Adicionado properties

--- core/core.go
+++ core/core.go
@@ -12,12 +12,12 @@ type WSDLDefinitions struct {
 	XMLName      xml.Name       `xml:"definitions"`
 	WSDLPortType []WSDLPortType `xml:"portType"`
 	WSDLMessage  []WSDLMessage  `xml:"message"`
-	WSDLTypes    []WSDLTypes    `xml:"types"`
+	WSDLTypes    WSDLTypes      `xml:"types"`
 }
 
 type WSDLTypes struct {
-	XMLName xml.Name  `xml:"types"`
-	SSchema []SSchema `xml:"schema"`
+	XMLName xml.Name `xml:"types"`
+	SSchema SSchema  `xml:"schema"`
 }
 
 type SSchema struct {
@@ -51,9 +51,9 @@ type WSDLPortType struct {
 }
 
 type WSDLMessage struct {
-	XMLName  xml.Name   `xml:"message"`
-	Name     string     `xml:"name,attr"`
-	WSDLPart []WSDLPart `xml:"part"`
+	XMLName  xml.Name `xml:"message"`
+	Name     string   `xml:"name,attr"`
+	WSDLPart WSDLPart `xml:"part"`
 }
 
 type WSDLPart struct {
@@ -66,7 +66,7 @@ type WSDLOperation struct {
 	XMLName xml.Name `xml:"operation"`
 	Name    string   `xml:"name,attr"`
 	Input   Input    `xml:"input"`
-	// Output  Output   `xml:"ouput"`
+	Output  Output   `xml:"output"`
 }
 
 type Input struct {
@@ -74,30 +74,27 @@ type Input struct {
 	Message string   `xml:"message,attr"`
 }
 
-// type Output struct {
-// 	XMLName xml.Name `xml:"output"`
-// 	Message string   `xml:"message,attr"`
-// }
-
-// Formly Types
-type Schema struct {
-	Title       string   `json:"title"`
-	Description string   `json:"description"`
-	Type        string   `json:"type"`
-	Required    []string `json:"required"`
-	// Properties  Properties `json:"properties"`
+type Output struct {
+	XMLName xml.Name `xml:"output"`
+	Message string   `xml:"message,attr"`
 }
 
-// type Properties struct {
-// 	PropertyName Property `json:""`
-// }
+// Formly types
 
-// type Property struct {
-// 	// TODO(Peter): Enumerar Type
-// 	Type    string `json:"type"`
-// 	Title   string `json:"title"`
-// 	Default string `json:"default"`
-// }
+type Schema struct {
+	Title       string              `json:"title"`
+	Description string              `json:"description"`
+	Type        string              `json:"type"`
+	Required    []string            `json:"required"`
+	Properties  map[string]Property `json:"properties"`
+}
+
+type Property struct {
+	// TODO(Peter): Enumerar Type
+	Type    string `json:"type"`
+	Title   string `json:"title"`
+	Default string `json:"default"`
+}
 
 func PrintJSON(wsdlByteArray []byte) {
 	var w WSDLDefinitions
@@ -112,8 +109,34 @@ func PrintJSON(wsdlByteArray []byte) {
 			s.Title = o.Name + " form"
 			s.Description = o.Name + " form example."
 			s.Type = "object"
+			s.Required = []string{}
+			s.Properties = make(map[string]Property)
 
-			s.Required = append(s.Required, o.Input.Message)
+			inputMessageValue := o.Input.Message[4:]
+
+			for m := 0; m < len(w.WSDLMessage); m++ {
+				if w.WSDLMessage[m].Name == inputMessageValue {
+
+					messagePartElement := w.WSDLMessage[m].WSDLPart.Element[4:]
+
+					for n := 0; n < len(w.WSDLTypes.SSchema.SElement); n++ {
+						if w.WSDLTypes.SSchema.SElement[n].Name == messagePartElement {
+							for e := 0; e < len(w.WSDLTypes.SSchema.SElement[n].SComplexType.SSequence.SElement); e++ {
+								// TODO(Peter): Como eu sei se o elemento é requerido?
+								element := w.WSDLTypes.SSchema.SElement[n].SComplexType.SSequence.SElement[e]
+
+								s.Properties[element.Name] = Property{
+									Type:  element.Type[2:],
+									Title: element.Name,
+								}
+
+							}
+						}
+					}
+
+					break
+				}
+			}
 
 			schemaData, _ := json.MarshalIndent(s, "", "    ")
 			schemaStr := string(schemaData)
@@ -122,10 +145,4 @@ func PrintJSON(wsdlByteArray []byte) {
 		}
 	}
 
-}
-
-func getDefaultFormlySchema() Schema {
-	return Schema{
-		Title: "Teste", Description: "Teste", Type: "object",
-	}
 }
